d923e375d70ae07374775d1afb5ea2fcd630145f
AppleRustria <arustria@myseneca.ca>
2024-11-01 00:06:12 -0400

    html and others

---
+++ public/assets/js/main.js
@@ -0,0 +1,5 @@
+// JavaScript for toggling mobile menu (if you add one later)
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('active');
+  });
+  
