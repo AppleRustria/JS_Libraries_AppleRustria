6e00814a6a2c2dd9aa0b80a092629b38424774d9
AppleRustria <arustria@myseneca.ca>
2024-11-02 04:51:36 -0400

    edit 6
    rename  images and add some revision

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,12 +1,21 @@
-// main.js
-
-// Ensure that the DOM is fully loaded before running the Glide initialization
 document.addEventListener("DOMContentLoaded", function() {
-    new Glide('.glide', {
+    var glide = new Glide('#hero', {
       type: 'carousel',
-      autoplay: 5000, // Auto-scroll every 5 seconds
+      perView: 1,           // Single slide view for the hero section
+      focusAt: 'center',
+      autoplay: 5000,       // Auto-scroll every 5 seconds
       hoverpause: true,
       animationDuration: 800,
-      perView: 1
-    }).mount();
+      breakpoints: {
+        800: {
+          perView: 1        // Single slide view at 800px and below for hero
+        },
+        480: {
+          perView: 1        // Single slide view at 480px and below for hero
+        }
+      }
+    });
+  
+    glide.mount();
   });
+  
