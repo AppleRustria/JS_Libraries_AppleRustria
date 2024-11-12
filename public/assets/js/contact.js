f36e37fc475f794d471501b2987f82e9c461e120
AppleRustria <arustria@myseneca.ca>
2024-11-12 04:39:31 -0500

    styling the code

--- public/assets/js/contact.js
+++ public/assets/js/contact.js
@@ -12,18 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 //map
 
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     var map = L.map('map').setView([51.505, -0.09], 13); // Replace with your coordinates
-
-//     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//         attribution: '© OpenStreetMap contributors'
-//     }).addTo(map);
-
-//     // Adding a marker
-//     var marker = L.marker([51.505, -0.09]).addTo(map); // Replace with your coordinates
-//     marker.bindPopup('<b>Hello world!</b><br>I am a popup.').openPopup();
-// });
+// leaf
 
 var map = L.map('map').setView([51.505, -0.09], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
